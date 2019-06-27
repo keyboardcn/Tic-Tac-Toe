b84e4530f11b71561a3fa48bf0fe1fadc18271b4
keyboardcn <liuliming.kz@gmail.com>
2019-06-27 14:51:47 -0600

    First commit

--- src/App.js
+++ src/App.js
@@ -2,23 +2,129 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function Square(props) {
+
+  return (
+    <button className='square' onClick={() => { props.onClick() }} >
+      {props.value}
+    </button>
+  );
+}
+
+
+class Board extends React.Component {
+
+  renderSquare(i) {
+    return (
+      <Square
+        value={this.props.squares[i]}
+        onClick={() => this.props.onClick(i)}
+      />
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        <div className='board-row'>
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className='board-row'>
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className='board-row'>
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    )
+  }
+}
+
+class Game extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      history: [{
+        squares: Array(9).fill(null)
+      }],
+      evenClick: true,
+      stepNumber: 0
+    };
+  }
+  handleOnClick(i) {
+    const history = this.state.history.slice(0, this.state.stepNumber + 1)
+    let squares = history[history.length - 1].squares.slice()
+    if (squares[i]) { return }
+    let step = this.state.stepNumber + 1
+    squares[i] = this.state.evenClick ? 'O' : 'X'
+    this.setState({
+      history: [...this.state.history, { squares: squares }],
+      evenClick: !this.state.evenClick,
+      stepNumber: step
+    })
+  }
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+    })
+  }
+  render() {
+    const history = this.state.history[this.state.stepNumber]
+    const winner = calculateWinner(this.state.history[this.state.history.length-1].squares)
+    let status = 'The next player is: '+(this.state.evenClick ? 'O' : 'X')
+    status = winner?'We have a winner: '+winner : status
+    const moves = this.state.history.map((step, move) => {
+      const desc = move ? 'go to step #: ' + move : 'go to start'
+      return (
+        <li key={move}>
+          <button onClick={() => this.jumpTo(move)}>
+            {desc}
+          </button>
+        </li>
+      )
+    })
+    return (
+      <div className='game'>
+        <Board squares={history.squares.slice()} onClick={i => this.handleOnClick(i)} />
+        <div className="game-info">
+          {status}
+          <ol>{moves}</ol>
+        </div>
+      </div>
+    )
+  }
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Game />
     </div>
   );
 }
